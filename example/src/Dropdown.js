ecf06807bba96e40bd4d9f70b699b6d8bea3c76d
guillaume gaillard <gaillard.guillaum@gmail.com>
2021-11-24 19:09:30 +0100

    dropdown in progress

--- example/src/Dropdown.js
+++ example/src/Dropdown.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import './dropdown.css'
+import arrow from './assets/arrow.png'
 
+const Dropdown = ({ array, id }) => {
 
-const Dropdown = ({array}) => {
+  const dropContentRef = useRef()
+
+  const toggleDropDown = () => {
+    const contentHeight = array.length * 25
+
+    if ( dropContentRef.current.classList.contains('collapse')) {
+      dropContentRef.current.classList.remove('collapse')
+      dropContentRef.current.animate([{height :'0px' },{height:`${contentHeight}px`}] ,{duration : 1000 ,animationFillMode: "forwards"} )
+      }
+    else {
+      dropContentRef.current.style.height = `${contentHeight}px`
+      dropContentRef.current.classList.add('collapse')
+     
+    }
+
+  }
   return (
-    <ul>
-      {array.map(function(item, i){
-      console.log('test');
-      return <li key = {i}>{item.name}</li>;
-    })}
-    </ul>
+    <div className="dropdown" >
+      <div className="dropdown-header dropdown-header-list">
+        <p>{array[0].name}</p>
+        <img src={arrow} alt="fléche" onClick={toggleDropDown} />
+      </div>
+      <div ref={dropContentRef} className="dropdown-content dropdown-content-list ">
+        <ul>{array.map((item, index) => (
+          <li key={index} >{item.name}</li>
+        ))}</ul>
+      </div>
+    </div>
   )
 }
 
