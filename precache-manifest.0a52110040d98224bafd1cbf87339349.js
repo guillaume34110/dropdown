d04c7c5a1fb0d32f328fbd62fa5eed8a999507f3
guillaume gaillard <gaillard.guillaum@gmail.com>
2021-11-25 09:42:36 +0100

    Updates

---
+++ precache-manifest.0a52110040d98224bafd1cbf87339349.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c249cae4c8b4d0369408ad0e445bb515",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed9d9f8537593c312635",
+    "url": "./static/css/main.b73f8e61.chunk.css"
+  },
+  {
+    "revision": "a88eae906fa65915a718",
+    "url": "./static/js/2.52e2f889.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.52e2f889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed9d9f8537593c312635",
+    "url": "./static/js/main.041dad1c.chunk.js"
+  },
+  {
+    "revision": "f2f0712884bd711f8762",
+    "url": "./static/js/runtime-main.c3512797.js"
+  }
+]);
